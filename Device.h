92dfc375e257237458cab03f8e32ebb55b1fc6cf
Jeff Glass <glass.jeffrey@gmail.com>
2021-01-21 13:55:30 -0600

    Create barebones device model (empty)

---
+++ Device.h
@@ -0,0 +1,32 @@
+/*
+  Device.h - DMX library for Arduino with RDM (Remote Device Management) support
+  Copyright (c) 2020 Jeff Glass (glass.jeffrey@gmail.com).  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef DEVICE_H_
+#define DEVICE_H_
+
+#include <inttypes.h>
+
+struct Device{
+
+    void Initialize(){
+
+    }
+};
+
+#endif /* DEVICE_H_ */
